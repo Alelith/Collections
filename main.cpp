9b90b6f29dfaf991cb63eee26d3c68fda6c9b1d7
Lilith Estévez Boeta <eacaymo@gmail.com>
2025-09-03 22:23:23 +0200

    Vectors implemented

---
+++ main.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include "vector.hpp"
+
+int main() {
+	Vector<int> vec;
+
+	std::cout << "Size: " << vec.size() << std::endl;
+	std::cout << "Capacity: " << vec.capacity() << std::endl;
+
+	vec.push_back(42);
+	std::cout << "Size after push_back: " << vec.size() << std::endl;
+	std::cout << "Capacity after push_back: " << vec.capacity() << std::endl;
+
+	vec.push_back(42);
+	std::cout << "Size after push_back: " << vec.size() << std::endl;
+	std::cout << "Capacity after push_back: " << vec.capacity() << std::endl;
+
+	vec.push_back(42);
+	std::cout << "Size after push_back: " << vec.size() << std::endl;
+	std::cout << "Capacity after push_back: " << vec.capacity() << std::endl;
+
+	vec.push_back(42);
+	std::cout << "Size after push_back: " << vec.size() << std::endl;
+	std::cout << "Capacity after push_back: " << vec.capacity() << std::endl;
+
+	vec.push_back(42);
+	std::cout << "Size after push_back: " << vec.size() << std::endl;
+	std::cout << "Capacity after push_back: " << vec.capacity() << std::endl;
+
+	vec.push_back(42);
+	std::cout << "Size after push_back: " << vec.size() << std::endl;
+	std::cout << "Capacity after push_back: " << vec.capacity() << std::endl;
+
+	vec.push_back(42);
+	std::cout << "Size after push_back: " << vec.size() << std::endl;
+	std::cout << "Capacity after push_back: " << vec.capacity() << std::endl;
+
+	vec.push_back(42);
+	std::cout << "Size after push_back: " << vec.size() << std::endl;
+	std::cout << "Capacity after push_back: " << vec.capacity() << std::endl;
+
+	vec.pop_back();
+	std::cout << "Size after pop_back: " << vec.size() << std::endl;
+	std::cout << "Capacity after pop_back: " << vec.capacity() << std::endl;
+
+	vec.pop_back();
+	std::cout << "Size after pop_back: " << vec.size() << std::endl;
+	std::cout << "Capacity after pop_back: " << vec.capacity() << std::endl;
+
+	vec.pop_back();
+	std::cout << "Size after pop_back: " << vec.size() << std::endl;
+	std::cout << "Capacity after pop_back: " << vec.capacity() << std::endl;
+
+	vec.pop_back();
+	std::cout << "Size after pop_back: " << vec.size() << std::endl;
+	std::cout << "Capacity after pop_back: " << vec.capacity() << std::endl;
+
+	vec.pop_back();
+	std::cout << "Size after pop_back: " << vec.size() << std::endl;
+	std::cout << "Capacity after pop_back: " << vec.capacity() << std::endl;
+
+	vec.pop_back();
+	std::cout << "Size after pop_back: " << vec.size() << std::endl;
+	std::cout << "Capacity after pop_back: " << vec.capacity() << std::endl;
+
+	vec.pop_back();
+	std::cout << "Size after pop_back: " << vec.size() << std::endl;
+	std::cout << "Capacity after pop_back: " << vec.capacity() << std::endl;
+
+	vec.pop_back();
+	std::cout << "Size after pop_back: " << vec.size() << std::endl;
+	std::cout << "Capacity after pop_back: " << vec.capacity() << std::endl;
+
+	vec.pop_back();
+	std::cout << "Size after pop_back: " << vec.size() << std::endl;
+	std::cout << "Capacity after pop_back: " << vec.capacity() << std::endl;
+
+	vec.pop_back();
+	std::cout << "Size after pop_back: " << vec.size() << std::endl;
+	std::cout << "Capacity after pop_back: " << vec.capacity() << std::endl;
+
+	vec.pop_back();
+	std::cout << "Size after pop_back: " << vec.size() << std::endl;
+	std::cout << "Capacity after pop_back: " << vec.capacity() << std::endl;
+
+	return 0;
+}

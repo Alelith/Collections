e3fd75c642345e0bb0e8fe2bba7f7df810ee7058
Lilith Estévez Boeta <eacaymo@gmail.com>
2025-09-08 21:44:43 +0200

    Deque script created

---
+++ include/deque.cpp
@@ -0,0 +1,134 @@
+#pragma once
+#include <iostream>
+
+template<class T>
+class Deque {
+public:
+	using value_type		= T;
+	using size_type			= unsigned long;
+	using reference			= T&;
+	using const_reference	= const T&;
+	using pointer			= T*;
+	using const_pointer		= const T*;
+	using iterator			= T*;
+	using const_iterator	= const T*;
+
+	Deque() noexcept : data_(nullptr), size_(0), capacity_(0) {}
+
+	Deque(size_type count, const T &value) : size_(count), capacity_(count), head_(0), tail_(0) {
+		data_ = new T[capacity_];
+		for (size_type i = 0; i < size_; ++i)
+			data_[i] = value;
+	}
+
+	Deque(const Deque &other) : size_(other.size_), capacity_(other.capacity_), head_(other.head_), tail_(other.tail_) {
+		data_ = new T[capacity_];
+		for (size_type i = 0; i < size_; ++i)
+			data_[i] = other.data_[i];
+	}
+
+	Deque(Deque &&other) noexcept : data_(other.data_), size_(other.size_), capacity_(other.capacity_), head_(other.head_), tail_(other.tail_) {
+		other.data_ = nullptr;
+		other.size_ = 0;
+		other.capacity_ = 0;
+		other.head_ = 0;
+		other.tail_ = 0;
+	}
+
+	~Deque() {
+		delete[] data_;
+	}
+
+	reference operator[](size_type index) {
+		return data_[(index + head_) % capacity_];
+	}
+
+	Deque &operator=(const Deque &other) {
+		if (this != &other) {
+			delete[] data_;
+			size_ = other.size_;
+			capacity_ = other.capacity_;
+			data_ = new T[capacity_];
+			for (size_type i = 0; i < size_; ++i)
+				data_[i] = other.data_[i];
+		}
+		return *this;
+	}
+
+	Deque &operator=(Deque &&other) noexcept {
+		if (this != &other) {
+			delete[] data_;
+			data_ = other.data_;
+			size_ = other.size_;
+			capacity_ = other.capacity_;
+			other.data_ = nullptr;
+			other.size_ = 0;
+			other.capacity_ = 0;
+		}
+		return *this;
+	}
+
+	void reserve(size_type new_cap) {
+		if (new_cap > capacity_) {
+			pointer new_data = new T[new_cap];
+			for (size_type i = 0; i < size_; ++i)
+				new_data[i] = std::move(data_[i]);
+			delete[] data_;
+			data_ = new_data;
+			capacity_ = new_cap;
+		}
+	}
+
+	void shrink_to_fit() {
+		if (size_ > 0) {
+			if (size_ <= capacity_) {
+				pointer new_data = new T[size_];
+				for (size_type i = 0; i < size_; ++i)
+					new_data[i] = std::move(data_[i]);
+				delete[] data_;
+				data_ = new_data;
+				capacity_ = size_;
+				tail_ %= capacity_;
+				head_ %= capacity_;
+			}
+		}
+		else
+		{
+			delete[] data_;
+			data_ = nullptr;
+			capacity_ = size_;
+			head_ = tail_ = 0;
+		}
+	}
+
+	void clear() noexcept {
+		size_ = 0;
+		shrink_to_fit();
+	}
+
+	const_reference at(size_type index) const {
+		if (index >= size_)
+			throw std::out_of_range("Index out of range");
+		return data_[index];
+	}
+
+	size_type size() const noexcept { return size_; }
+	size_type capacity() const noexcept { return capacity_; }
+	size_type head() const noexcept { return head_; }
+	size_type tail() const noexcept { return tail_; }
+	bool empty() const noexcept { return size_ == 0; }
+	pointer data() noexcept { return data_; }
+	const_pointer data() const noexcept { return data_; }
+
+	/*iterator begin() noexcept { return data_; }
+	const_iterator begin() const noexcept { return data_; }
+	iterator end() noexcept { return data_ + size_; }
+	const_iterator end() const noexcept { return data_ + size_; }*/
+
+private:
+	pointer		data_;
+	size_type	size_;
+	size_type	capacity_;
+	size_type	head_;
+	size_type	tail_;
+};
